931eca9b320ee0c14cc0553010e25248b97e552f
Macbook <macbook@MacBook-Pro-6.local>
2022-10-20 21:48:44 -0700

    Make changes to the spider

--- Gas_Scraper/gasscraper/gasscraper/spiders/scraper.py
+++ Gas_Scraper/gasscraper/gasscraper/spiders/scraper.py
@@ -1,15 +1,16 @@
 import scrapy
 
-class whiskycrapy(scrapy.Spider):
-    name='whisky'
-    start_urls = ['https://www.caskers.com/spirits/whiskey/']
+class gas(scrapy.Spider):
+    name='gas'
+    
+    def start_requests(self):
+        yield scrapy.Request('https://www.gasbuddy.com/home?search=grants+pass&fuel=1&method=all&maxAge=0')
 
     def parse(self, response):
-        products = response.css('li.item product product-item')
+        products = response.css('div.GenericStationListItem-module__stationListItem___3Jmn4')
         for product in products:
-            item = {
-            'brand': response.css('a.product-item-link::text').get(),
-            'price': response.css('span.price::text').get().replace('$','')
+            yield {
+            'gas_station': response.css('h3.header__header3___1b1oq.header__header___1zII0.header__midnight___1tdCQ.header__snug___lRSNK.StationDisplay-module__stationNameHeader___1A2q8>a::text').get(),
+            'price': response.css('span.text__xl___2MXGo.text__left___1iOw3.StationDisplayPrice-module__price___3rARL::text').get().replace('$',''),
+            'address': response.css('div.StationDisplay-module__address___2_c7v::text').get()
             }
-            yield item
-        pass
