fc00b41143e8c7f90270758d0e90c611327385f1
Macbook <macbook@MacBook-Pro-6.local>
2022-10-23 15:48:23 -0700

    Made changes to the gas scraper project

--- Gas_Scraper/evscraper/gasscraper/spiders/scraper.py
+++ Gas_Scraper/evscraper/gasscraper/spiders/scraper.py
@@ -11,6 +11,9 @@ class gasscraper(scrapy.Spider):
                 'name': station.css('h3.header__header3___1b1oq.header__header___1zII0.header__midnight___1tdCQ.header__snug___lRSNK.StationDisplay-module__stationNameHeader___1A2q8>a::text').get(),
                 'price': station.css('span.text__xl___2MXGo.text__left___1iOw3.StationDisplayPrice-module__price___3rARL::text').get().replace('$',''),
                 'price_submitted': station.css('span.ReportedBy-module__postedTime___J5H9Z::text').get(),
-                'address': station.css('div.StationDisplay-module__address___2_c7v').get()
+                'address': station.css('div.StationDisplay-module__address___2_c7v::text').get()
 
             }
+            next_page = response.css('a.button__button___fo2tk.forms__field___E4Q71.forms__formControlBase___3Cl7I.button__fluid___2ez5a.button__secondary___1xuZs.button__branded___3hDeX').attrib['href']
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
